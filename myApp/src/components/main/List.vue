2bea62a0223e49c76a9f9047c74f9e2f81dc5bbc
NingGuoGit <844714367@qq.com>
2019-08-14 17:28:35 +0800

    小马老师辛苦啦

--- myApp/src/components/main/List.vue
+++ myApp/src/components/main/List.vue
@@ -1,6 +1,31 @@
 <template>
     <div>
       <Head/>
+      <h3>官方榜</h3>
+      <div class="list">
+        <ul>
+          <li v-for="item of arr">
+            <a href="">
+              <img src="./../../assets/images/3.jpg" alt="">
+              <span>{{item}} </span>
+            </a>
+            <div class="sing">
+              <div class="div" v-for="(items,index) of list">
+                <h4>{{index+1}}.{{items}} </h4>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <h3>全球榜</h3>
+      <van-grid :border="false" :column-num="3">
+        <ul>
+          <li v-for="it of lis" class="lis">
+            <img src="./../../assets/images/5.jpg" alt="">
+            <span>{{it}} </span>
+          </li>
+        </ul>
+      </van-grid>
     </div>
 </template>
 
@@ -8,10 +33,61 @@
   import Head from './../small/Head'
   export default {
       name: "List",
-      components:{Head}
+      components:{Head},
+      data(){
+        return{
+          arr:['飙升榜','新歌榜','原创榜','热歌榜'],
+          list:['红豆--王菲','飞云之下--林俊杰/韩红','慢慢喜欢你--莫文蔚'],
+          lis:['说唱榜','古典音乐榜','电音榜','新声榜','音乐榜','国电榜']
+        }
+      }
   }
 </script>
 
 <style scoped>
-
+  h3{
+    font-size: 0.36rem;
+    margin: 0.10rem 0 0 0.30rem;
+  }
+  a span{
+    font-size: 0.32rem;
+    position: absolute;
+    bottom:0.3rem;
+    right:0;
+  }
+  li a{
+    position: relative;
+    float: left;
+  }
+  .sing{
+    float: left;
+    margin-left: 0.50rem;
+  }
+  .sing h4{
+    line-height: 0.70rem;
+  }
+  img{
+    width: 2rem;
+    height: 2rem;
+  }
+  ul{
+    padding-left: 0.30rem;
+  }
+  li{
+    height: 2.5rem;
+  }
+  h4{
+    font-size:0.30rem;
+  }
+  .lis{
+    position: relative;
+    float: left;
+    margin-right: 0.30rem;
+  }
+  .lis span{
+    font-size:0.32rem;
+    position: absolute;
+    bottom:0.5rem;
+    right:0;
+  }
 </style>
--- myApp/src/components/main/Singer.vue
+++ myApp/src/components/main/Singer.vue
@@ -2,18 +2,19 @@
     <div>
       <Head/>
       <van-tabs>
-        <van-tab v-for="item of list" :title="item">
+        <van-tab v-for="item of list" :title="item.title">
           <ul>
-            <li v-for="item of singer">
+            <li v-for="items of item.name">
               <a href="">
                 <img src="./../../assets/images/2.jpg" alt="">
-                <span>{{item}}</span>
+                <span>{{items}}</span>
               </a>
             </li>
           </ul>
         </van-tab>
         <van-index-bar></van-index-bar>
       </van-tabs>
+
     </div>
 </template>
 
@@ -24,9 +25,17 @@
       components:{Head},
       data() {
         return {
-          activeName: 'a',
-          list:['分类(默认热门)','华语男','华语女','华语组合','欧美男','欧美女','欧美组合'],
-          singer:['林俊杰','陈奕迅','周杰伦','邓紫棋','薛之谦','花粥','李荣浩','华晨宇','周深']
+          // list:['分类(默认热门)','华语男','华语女','华语组合','欧美男','欧美女','欧美组合'],
+          // singer:['林俊杰','陈奕迅','周杰伦','邓紫棋','薛之谦','花粥','李荣浩','华晨宇','周深'],
+
+          list:[
+            {title:'分类(默认热门)',name:['邓紫棋','薛之谦','花粥','李荣浩','华晨宇','周深']},
+            {title:'华语男',name:['林俊杰','陈奕迅','周杰伦','林宥嘉','薛之谦','毛不易','李荣浩','华晨宇','周深']},
+            {title:'华语女',name:['田馥甄','王菲','刘若英','A-lin','莫文蔚','孙燕姿','邓紫棋','花粥']},
+            {title:'华语组合',name:['SHE','苏打绿','F.I.R','牛奶咖啡','五月天','Beyond','逃跑计划','声入人心男团','好妹妹']},
+            {title:'欧美男',name:['Justin Bieber','Christine Welch','The Chainsmokers','Billie Eilish','Coldplay','Maroon 5']},
+          ],
+
         }
       }
   }
@@ -38,6 +47,9 @@
   }
   ul{
     padding-left: 0.30rem;
+  }
+  li{
+    border-bottom: 1px solid #ccc;
   }
   a{
     height: 2rem;
