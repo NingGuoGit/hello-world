e23c7dba05c2582251fdc705b1db766a941caac2
NingGuoGit <844714367@qq.com>
2019-08-14 14:08:18 +0800

    冲突解决

--- myApp/src/components/zujian/Head.vue
+++ myApp/src/components/zujian/Head.vue
@@ -3,13 +3,18 @@
       <Header>
         <van-nav-bar title="云音悦" left-text="" left-arrow>
           <van-icon name="wap-nav" slot="left" />
-          <van-icon name="search" slot="right"></van-icon>
+          <van-icon name="search" slot="right" @click="search"></van-icon>
         </van-nav-bar>
         <nav>
           <router-link to="/">推荐</router-link>
           <router-link to="/singer">歌手</router-link>
           <router-link to="/list">排行榜</router-link>
         </nav>
+        <!-- <van-tabs v-model="active">
+          <van-tab title="推荐" @click="tuijian"></van-tab>
+          <van-tab title="歌手" @click="geshou"></van-tab>
+          <van-tab title="排行榜" @click="paihang"></van-tab>
+        </van-tabs> -->
       </Header>
     </div>
 </template>
@@ -22,8 +27,21 @@
       return {
         active: 2,
       };
+    },
+    methods:{
+      search(){
+        this.$router.push('search')
+      },
+      // tuijian(){
+      //   this.$router.push({path:'home'})
+      // },
+      // gehsou(){
+      //   this.$router.push({path:'singer'})
+      // },
+      // paihang(){
+      //   this.$router.push({path:'list'})
+      // }
     }
-
   }
 
 </script>
